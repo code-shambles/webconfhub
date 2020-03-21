303b95b250953a8faac3231bfb37101c8c14a993
Sebastian Eck <codeshambles@gmail.com>
2020-03-21 10:29:15 +0100

    change room link into a big tile

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { settleRegistration } from "../../../redux/actions";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { settleRegistration } from '../../../redux/actions';
 
-import { Button, Message, Spinner } from "../../";
+import { Button, Message, Spinner } from '../../';
 
-import "./HomePage.less";
+import './HomePage.less';
 
 const userHasRoom = (room, invitations) => {
   return invitations && !!invitations[room.livewebinarId];
@@ -16,16 +16,16 @@ const renderRegistrations = (rooms, invitations) => {
     const messages = rooms.map(room => {
       if (invitations[room.livewebinarId]) {
         switch (invitations[room.livewebinarId].registrationStatus) {
-          case "fresh": {
+          case 'fresh': {
             return (
               <Message key={room.id}>
-                {" "}
-                <Spinner /> Registering you for room{" "}
+                {' '}
+                <Spinner /> Registering you for room{' '}
                 <strong>{room.name}</strong>...
               </Message>
             );
           }
-          case "success": {
+          case 'success': {
             return (
               <Message
                 key={room.id}
@@ -33,7 +33,7 @@ const renderRegistrations = (rooms, invitations) => {
                 doom={{
                   ttl: 5000,
                   onDeath: () =>
-                    settleRegistration(room.livewebinarId, invitations)
+                    settleRegistration(room.livewebinarId, invitations),
                 }}
               >
                 <i className="lni lni-checkmark-circle"></i> Registration for
@@ -41,7 +41,7 @@ const renderRegistrations = (rooms, invitations) => {
               </Message>
             );
           }
-          case "error": {
+          case 'error': {
             return (
               <Message key={room.id} type="error">
                 <i className="lni lni-warning"></i> Error registering you for
@@ -50,7 +50,7 @@ const renderRegistrations = (rooms, invitations) => {
               </Message>
             );
           }
-          case "settled":
+          case 'settled':
           default: {
             return null;
           }
@@ -78,7 +78,7 @@ const renderRooms = (rooms, invitations) =>
             <Link to={`/room/${room.id}`}>
               <i
                 className={`lni lni-${
-                  room.type === "breakout" ? "users" : "display-alt"
+                  room.type === 'breakout' ? 'users' : 'display-alt'
                 }`}
               ></i>
               <h3>{room.name}</h3>
@@ -95,6 +95,21 @@ const renderRooms = (rooms, invitations) =>
     <Message type="error">Config error: No streaming rooms found!</Message>
   );
 
+const renderZoomRooms = (rooms, invitations) => (
+  <ul>
+    <li className={`wch-room-tile`}>
+      <a href="https://sap-se.zoom.com/j/7450845795" target="_blank">
+        <i className={`lni lni-display-alt`}></i>
+        <h3>Saturn</h3>
+        <p>Our track Saturn on Zoom!</p>
+        <Button type="black">
+          ENTER <i className="lni lni-angle-double-right"></i>
+        </Button>
+      </a>
+    </li>
+  </ul>
+);
+
 const HomePage = ({ baseConfig, rooms, invitations }) => {
   return (
     <main id="wch-home" className="wch-main">
@@ -105,11 +120,7 @@ const HomePage = ({ baseConfig, rooms, invitations }) => {
       {renderRegistrations(rooms, invitations)}
       <section className="wch-rooms">
         <h2>Join us on Zoom</h2>
-        <p>
-          <a href="https://sap-se.zoom.com/j/7450845795" target="_blank">
-            Saturn Track
-          </a>
-        </p>
+        {renderZoomRooms()}
       </section>
     </main>
   );
@@ -120,7 +131,7 @@ const mapStateToProps = (state, ownProps) => {
     app: { ...state.app },
     baseConfig: { ...state.config.baseConfig },
     invitations: { ...state.invitations },
-    rooms: [...state.config.rooms]
+    rooms: [...state.config.rooms],
   };
 };
 
